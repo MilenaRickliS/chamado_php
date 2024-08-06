d93090c5cd0dfc83c2b12071ba1fa7f6cec9cd39
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-08-06 19:43:02 -0300

    mm

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -9,8 +9,20 @@ class LoginController{
     public function __construct(){
         $this -> usuarios = [
             new User(1, 'adm@teste.com', '1234', 1),
+            new User(2, 'jose@teste.com', '1234', 2),
         ];
     } 
+
+    public function autenticar($email, $senha){
+        foreach ($this-> usuarios as $user){
+           if($user->email == $email && $user->verificarSenha($senha)){
+                $_SESSION['autenticacao'] = 'SIM';
+                $_SESSION['id'] = $user->id;
+                $_SESSION['tipo'] = $user->tipo;  
+                header('Location: ../chamado/src/view/home.php');          
+           } 
+        }
+    }
 }
 
 ?>
