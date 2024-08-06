a804e34a8a7112595e5d3cdfd8d38c30a1a4e96e
MilenaRickliS <engs-milenasilverio@camporeal.edu.br>
2024-08-06 19:32:02 -0300

    cc

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -1,12 +1,14 @@
 <?php 
 
+require_once '../chamado/src/models/User.php';
+
 class LoginController{
 
     private $usuarios;
 
     public function __construct(){
         $this -> usuarios = [
-            
+            new User(1, 'adm@teste.com', '1234', 1),
         ];
     } 
 }
