cbc881ce608ad9f98203de3a6b1deb0d6da0e6e2
MilenaRickliS <engs-milenasiverio@camporeal.edu.br>
2024-08-13 19:45:51 -0300

    commit

--- valida_login.php
+++ valida_login.php
@@ -1,6 +1,10 @@
 <?php
 
+session_start();
+require_once '../chamado_php/src/controllers/LoginController.php';
 
+$loginController = new LoginController();
+$loginController->autenticar($_POST['email'], $_POST['senha']);
 
 ?>
 
